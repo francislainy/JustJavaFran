911bface9fc10e3f39db081919e96980c53331d5
Francislainy <francislainy.campos@gmail.com>
2016-12-26 09:08:12 +0000

    Added Listener to buttons

--- app/src/main/java/com/example/android/justjavafran/MainActivity.java
+++ app/src/main/java/com/example/android/justjavafran/MainActivity.java
@@ -2,10 +2,10 @@ package com.example.android.justjavafran;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,38 +16,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button playButton = (Button) findViewById(R.id.button_plus);
-        playButton.setOnClickListener(new View.OnClickListener() {
+        Button buttonPlus = (Button) findViewById(R.id.button_plus);
+        buttonPlus.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                display(v);
+            public void onClick(View view) {
+                quantity++;
+                display(view);
             }
         });
 
+        Button buttonMinus = (Button) findViewById(R.id.button_minus);
+        buttonMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(quantity > 1){
+                    quantity--;
+                }
+                if(quantity == 1)
+                    Toast.makeText(MainActivity.this, "Minimum number of cups of coffee reached", Toast.LENGTH_SHORT).show();
+                display(view);
+            }
+        });
+        
+
     }
 
-
-
-
-
-
-//        final TextView textView = (TextView) findViewById(R.id.display_order_view);
-//        Button buttonPlus = (Button) findViewById(R.id.button_plus);
-//        buttonPlus.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                quantity++;
-//                Log.v("MainActivity", "quantity " + quantity);
-//                textView.setText(quantity+"");
-//            }
-//        });
-
     public void display(View view) {
-        Log.v("MainActivity", "quantity " + quantity);
-        quantity++;
         TextView textView = (TextView) findViewById(R.id.display_order_view);
         textView.setText(quantity + "");
     }
 
 }
-
